24cf99712ab41af689a590fed90341853969e82a
Noah Byers <noahxbyers@gmail.com>
2022-12-02 15:03:16 -0800

    Add createTitle and buildMainCharacter

--- src/book.js
+++ src/book.js
@@ -1,11 +1,23 @@
-function createTitle() {
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`
   
+};
+function buildMainCharacter(charName,charAge,charPronouns) {
+var characterTraits = {
+  name:`${charName}`,
+  age: `${charAge}`,
+  pronouns:`${charPronouns}`
+ } 
+ return characterTraits
 }
 
+function saveReview() {
+
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
